2053b3f7eb2937c31e5f4b88d6a94d2b5474a021
Jah Seng Lee <jahseng619@gmail.com>
2016-05-09 00:02:32 +1200

    added more hidden pages + refactored some pages into seperate folders

--- marketplace_client/routes/index.js
+++ marketplace_client/routes/index.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+// ------------
+// Search pages
 router.get('/advanced', function(req, res) {
-  res.render('advancedSearch')
+  res.render('search/advancedSearch')
 });
 
+router.get('/singleItem', function(req, res) {
+  res.render('search/singleItem?displayItem=1')
+});
+
+// -------------
+// Account pages
 router.get('/account', function(req, res) {
-  res.render('account')
+  res.render('account/account')
+});
+
+router.get('/login', function(req, res) {
+  res.render('account/login')
+});
+
+router.get('/upload', function(req, res) {
+  res.render('account/upload')
 });
 
 module.exports = router;
