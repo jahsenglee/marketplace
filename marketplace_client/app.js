8aae590fdc836d4e694d997e23a954a90a71cd6b
Jah Seng Lee <jahseng619@gmail.com>
2016-05-20 05:16:11 +1200

    getAll function returns all items in stock

--- marketplace_client/app.js
+++ marketplace_client/app.js
@@ -5,15 +5,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoClient = require('mongodb').MongoClient;
+var mongoItems;
 
 var routes = require('./routes/index');
 
 var app = express();
 
 // Connect to the db
-mongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+mongoClient.connect("mongodb://localhost:27017/test", {strict: true}, function(err, db) {
   if(!err) {
     console.log("connected to mongo");
+    mongoItems = db.collection('items');
+
   } else {
     console.log("failed to connect to mongo");
   }
@@ -34,6 +37,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 // routes for mongo db
+function getAll() {
+  mongoItems.find().toArray(function(err, items) {
+    if (!err) {
+      // console.log(items[0].stock);
+      return items[0].stock; // returns all items
+    }
+  });
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
